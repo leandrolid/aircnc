a3673fc32f61323389eed8720314f27feb573159
Leandro Liduvino <leandro.augusto460@gmail.com>
2020-03-03 15:36:21 -0300

    First Readme.md

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# aircnc
+Projeto resultado da Semana OmniStack #9
+
+* Este projeto não foi refatorado, antãonão deve ser utilizado para benchmark.
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const sockectio = require('socket.io');
+const http = require('http');
 
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = sockectio(server);
 
 mongoose.connect('mongodb+srv://laslino:laslino@mongoomni-2w3ud.mongodb.net/aircnc?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,    
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const {user_id} = socket.handshake.query;
+    connectedUsers[user_id] = socket.id;
+
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 });
 
 app.use(cors());
@@ -17,5 +36,5 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
 
